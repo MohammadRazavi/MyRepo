06cda45105a349a53f568d663abd5ae73d9896fd
MoRiz <m.sayedrazavi@gmail.com>
2024-02-26 02:22:05 -0500

    test git

---
+++ test_python.py
@@ -0,0 +1,10 @@
+class Name:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f'His age is {self.age} and his name is {self.name}')
+
+name_id = Name("Mohammad",14)
+print(name_id.display_info())
